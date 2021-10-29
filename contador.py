550011dede3f1ecab16b37ec2188a136b73d37ce
Diego Socrates Dias Mousine <diemousine@hotmail.com>
2021-10-29 10:47:12 -0300

    Revert "Update contador.py"
    This reverts commit 2f83f065363c322ac59a816d42446b0a05cc0d9f.

--- contador.py
+++ contador.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
     wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
-#teste
